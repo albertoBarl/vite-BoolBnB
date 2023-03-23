e96edd448222715abc6674c09a4b37bfe8b2ca71
albertoBarl <alberto.barletta00@gmail.com>
2023-03-23 16:32:56 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+// bootstrap
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+
+createApp(App).mount("#app");
