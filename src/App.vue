04ecd8f6b2e318fca8645acef17b6482534aaffb
EsterSPR <web.esterspreafico@gmail.com>
2023-03-23 17:05:16 +0100

    standard .vue and .scss files created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+    components: {
+        AppHeader,
+        AppMain
+    }
+}
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="">
+  
+    <AppHeader />
+
+    <AppFooter />
+        
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './styles/generals.scss' as *;
+@use './styles/partials/variables.scss' as *;
+
+    
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    
+</template>
+
+<style lang="">
+    
+</style>
