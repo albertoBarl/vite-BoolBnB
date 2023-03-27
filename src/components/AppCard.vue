7b1e971337487d51ae7c23908c2f165686afaf8b
EsterSPR <web.esterspreafico@gmail.com>
2023-03-27 12:25:41 +0200

    created example card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+
+    <div class="card my_card">
+        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <p class="card-title">
+                <div class="row">
+                    <div class="col-7"><h5>Bois-en-Ardres, Ardres, Francia</h5></div>
+                    <div class="col-5 text-end"><fa icon="star" class="me-2" /> 4,91</div>
+                </div>
+            </p>
+            <p class="card-text">Host professionista <br> 16 - 21 apr</p>
+            <p class="card-text"><strong>118 €</strong> a notte </p>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+    
+.my_card{
+    width: 300px;
+    margin: 0 auto;
+
+    img {
+        object-fit: cover;
+        width: 300px;
+        height: 300px;
+    }
+}
+
+</style>
