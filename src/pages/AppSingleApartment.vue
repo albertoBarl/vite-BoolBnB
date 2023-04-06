c6a54e47d46cdbb829090a7813d49a6dcb4e102a
albertoBarl <alberto.barletta00@gmail.com>
2023-04-06 20:05:03 +0200

    sponsorship to solve

--- src/pages/AppSingleApartment.vue
+++ src/pages/AppSingleApartment.vue
@@ -17,6 +17,7 @@ export default {
       .get(`${this.baseUrl}/api/apartments/${this.$route.params.slug}`)
       .then((response) => {
         this.apartment = response.data.results;
+        console.log(this.apartment.sponsors);
         this.loading = false;
       });
   },
@@ -24,123 +25,205 @@ export default {
 </script>
 
 <template lang="">
+  <div class="container p-sm-0 p-lg-5">
+    <div class="col-12 d-flex justify-content-center" v-if="loading">
+      <div class="loader"></div>
+    </div>
 
-    <div class="container p-sm-0 p-lg-5">
-
-        <div class="col-12 d-flex justify-content-center" v-if="loading">
-            <div class="loader"></div>
+    <div v-else class="row">
+      <div class="row">
+        <div class="col-12">
+          <a href="http://localhost:5173/"
+            ><button class="btn mb-4 my_sellerbtn">
+              Torna alla lista <fa icon="reply" class="ms-1" /></button
+          ></a>
         </div>
+      </div>
 
-        <div v-else class="row">
+      <div
+        v-if="apartment.image.includes('post_images')"
+        class="col-lg-6 col-sm-12 justify-content-end"
+      >
+        <img
+          class="my_aptimg"
+          :src="`${this.baseUrl}/storage/${apartment.image}`"
+          alt=""
+        />
+      </div>
+      <div v-else class="col-lg-6 col-sm-12 justify-content-end">
+        <img class="my_aptimg" :src="`${apartment.image}`" alt="" />
+      </div>
 
-            <div class="row">
-              <div class="col-12">
-                <a href="http://localhost:5173/"><button class="btn mb-4 my_sellerbtn">Torna alla lista <fa icon="reply" class="ms-1" /></button></a>
-              </div>
-            </div>
-
-            <div v-if="apartment.image.includes('post_images')" class="col-lg-6 col-sm-12 justify-content-end">
-                <img class="my_aptimg" :src="`${this.baseUrl}/storage/${apartment.image}`" alt="">
-            </div>
-            <div v-else class="col-lg-6 col-sm-12 justify-content-end">
-                <img class="my_aptimg" :src="`${apartment.image}`" alt="">
-            </div>
-
-            <div class="col-lg-6 col-sm-12 justify-content-start">
-
-                <div class="row mt-4"> 
-                    <div class="col-sm-9 col-lg-12 text-wrap">
-                        <h2>{{ apartment.title }}</h2> 
-                        <p>{{ apartment.address }}</p>
-                    </div>
-                    <!-- <div class="col-sm-3 col-lg-3 text-end">
+      <div class="col-lg-6 col-sm-12 justify-content-start">
+        <div class="row mt-4">
+          <div class="col-sm-9 col-lg-12 text-wrap">
+            <h2>
+              {{ apartment.title }}
+            </h2>
+            <!-- <span class="btn btn-dark">{{
+                apartment.sponsors[0].title
+              }}</span> -->
+            <p>{{ apartment.address }}</p>
+          </div>
+          <!-- <div class="col-sm-3 col-lg-3 text-end">
                         <div class="my_aptprice"><h5 class="me-3">500€</h5></div>
                     </div>
                     <div class="col-lg-9"></div> -->
-                </div>
-                
-                <div class="row mt-2 text-center">
-                    <div class="col-4 border-end align-items-center p-2">
-                        <fa :icon="['fab', 'codepen']" class="me-2 my_apticon" /> {{ apartment.square_feet }}&#178;
-                    </div>
-                    <div class="col-4 border-end align-items-center p-2">
-                        <fa icon="bed" class="me-2 my_apticon" /> {{ apartment.room}}
-                    </div>
-                    <div class="col-4 align-items-center p-2">
-                        <fa icon="person-half-dress" class="me-2 my_apticon" /> {{ apartment.bathroom }}
-                    </div>
-                </div>
-
-                <div class="row my_desclg">
-                    <div class="row mt-4 text-wrap my_justify">
-                        <p>{{ apartment.description }}</p>
-                        <button class="btn mt-4 w-100 my_sellerbtn" data-bs-toggle="modal" data-bs-target="#exampleModal">Contatta il venditore</button>
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Contatta il venditore!</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                              </div>
-                              <div class="modal-body">
-                                <div>
-                                  <div class="input-group mb-3">
-                                    <span class="input-group-text my_sellerbtn" id="basic-addon1">@</span>
-                                    <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1">
-                                  </div>
-                                  <div>
-                                    <div class="input-group">
-                                      <span class="input-group-text my_sellerbtn">Contenuto</span>
-                                      <textarea class="form-control" aria-label="With textarea"></textarea>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                <button type="button" class="btn btn-primary my_sellerbtn">Invia Email al venditore!</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
         </div>
 
-        <div class="row mt-4 text-wrap my_justify my_descsm">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aspernatur cumque nostrum laudantium tenetur velit voluptatem dolorem deleniti hic deserunt labore, iure, dolorum laborum officia ab et fugit quas suscipit.</p>
-            <a href="#"><button class="btn mt-4 w-100 my_sellerbtn">Contatta il venditore</button></a>
-            <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+        <div class="row mt-2 text-center">
+          <div class="col-4 border-end align-items-center p-2">
+            <fa :icon="['fab', 'codepen']" class="me-2 my_apticon" />
+            {{ apartment.square_feet }}&#178;
+          </div>
+          <div class="col-4 border-end align-items-center p-2">
+            <fa icon="bed" class="me-2 my_apticon" /> {{ apartment.room }}
+            {{ apartment.sponsorship }}
+          </div>
+          <div class="col-4 align-items-center p-2">
+            <fa icon="person-half-dress" class="me-2 my_apticon" />
+            {{ apartment.bathroom }}
+          </div>
+        </div>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="row my_desclg">
+          <div class="row mt-4 text-wrap my_justify">
+            <p>{{ apartment.description }}</p>
+            <button
+              class="btn mt-4 w-100 my_sellerbtn"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              Contatta il venditore
+            </button>
+            <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                      Contatta il venditore!
+                    </h5>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body">
+                    <div>
+                      <div class="input-group mb-3">
+                        <span
+                          class="input-group-text my_sellerbtn"
+                          id="basic-addon1"
+                          >@</span
+                        >
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Email"
+                          aria-label="Username"
+                          aria-describedby="basic-addon1"
+                        />
+                      </div>
+                      <div>
+                        <div class="input-group">
+                          <span class="input-group-text my_sellerbtn"
+                            >Contenuto</span
+                          >
+                          <textarea
+                            class="form-control"
+                            aria-label="With textarea"
+                          ></textarea>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Annulla
+                    </button>
+                    <button type="button" class="btn btn-primary my_sellerbtn">
+                      Invia Email al venditore!
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+
+    <div class="row mt-4 text-wrap my_justify my_descsm">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+        aspernatur cumque nostrum laudantium tenetur velit voluptatem dolorem
+        deleniti hic deserunt labore, iure, dolorum laborum officia ab et fugit
+        quas suscipit.
+      </p>
+      <a href="#"
+        ><button class="btn mt-4 w-100 my_sellerbtn">
+          Contatta il venditore
+        </button></a
+      >
+      <!-- Button trigger modal -->
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-        </div>
-
-
-    </div>
-
 </template>
 
 <style lang="scss" scoped>
@@ -232,5 +315,4 @@ export default {
     object-fit: cover;
   }
 }
-
 </style>
